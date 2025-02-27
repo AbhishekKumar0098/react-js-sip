ec2b203de06f8095d750e1f7aaa8009787e43c59
Yash Jain <yash.jain@capanicus.com>
2025-02-27 12:03:53 +0530

    changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,8 @@
 // Your utility functions and Modal component here
+import React, { useState } from "react";
 export const add = (a: number, b: number) => a + b;
 export const subtract = (a: number, b: number) => a - b;
+export const Number = () => {
+  const [first, setfirst] = useState("Yash");
+  return <div>{first}</div>;
+};
