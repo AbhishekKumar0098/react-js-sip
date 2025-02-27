1affe802a051a2023ea51e709508aa60d6f94a67
Yash Jain <yash.jain@capanicus.com>
2025-02-27 12:48:31 +0530

    changes

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 export const add = (a: number, b: number) => a + b;
 export const subtract = (a: number, b: number) => a - b;
-export const Number = () => {
+export const Number2 = () => {
   const [first, setfirst] = useState("Yash");
   return <div>{first}</div>;
 };
