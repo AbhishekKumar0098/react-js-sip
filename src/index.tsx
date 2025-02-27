7575a6612553f6aae0797e4518af10fa405aaa3a
Yash Jain <yash.jain@capanicus.com>
2025-02-27 12:55:41 +0530

    changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,6 @@
 // Your utility functions and Modal component here
 import React, { useState } from "react";
-export const add = (a: number, b: number) => a + b;
-export const subtract = (a: number, b: number) => a - b;
-
-export const Number = () => {
+export const Name = () => {
   const [first, setFirst] = useState("Yash");
   return (
     <div>
@@ -17,4 +14,4 @@ export const Number = () => {
 };
 
 // Export the component to be accessed globally
-export default Number;
+export default Name;
