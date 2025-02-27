caa287dccde8bf2c16e252efa61380404c5b786a
Yash Jain <yash.jain@capanicus.com>
2025-02-27 12:54:46 +0530

    changes

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,19 @@
 import React, { useState } from "react";
 export const add = (a: number, b: number) => a + b;
 export const subtract = (a: number, b: number) => a - b;
-export const Number2 = () => {
-  const [first, setfirst] = useState("Yash");
-  return <div>{first}</div>;
+
+export const Number = () => {
+  const [first, setFirst] = useState("Yash");
+  return (
+    <div>
+      {first}
+      <button
+        onClick={() => {
+          setFirst("Abhishek");
+        }}>Change Name</button>
+    </div>
+  );
 };
+
+// Export the component to be accessed globally
+export default Number;
